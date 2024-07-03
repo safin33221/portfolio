ea561e3fb75d0ede47e2452c6737fcf0a53fbbd9
safin33221 <saifn33221@gmail.com>
2024-07-03 16:24:46 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+var typed = new Typed(".typing",{
+    strings:[ "","Student.","Web Desinger.","web devoloper.","YouTuber.","BugHunter."],
+    typeSpeed:150,
+    BackSpeed:100,
+    loop:true
+})
