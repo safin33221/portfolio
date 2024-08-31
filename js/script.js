0255f537ed68457b5be9289a32b67866d9088bd7
safin33221 <saifn33221@gmail.com>
2024-08-31 18:39:35 +0600

    update contact section

--- js/script.js
+++ js/script.js
@@ -11,3 +11,21 @@ function hireme(){
     var name = prompt()
     alert("welcome " + name + ". carry on your process")
 }
+
+function submitMsg(){
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+    const success = document.getElementById('success') ;
+    document.getElementById('hidden').style.display = "none";
+    document.getElementById("result").style.display ="block"
+
+    document.getElementById('names').innerHTML ="Your Name : " + name;
+    document.getElementById('emails').innerHTML =" Your Email : "+ email;
+    document.getElementById('subjects').innerHTML =" Subject : "+ subject;
+    document.getElementById('messages').innerHTML =" Message : "+ message;
+    document.getElementById('succes').innerHTML ="submit has been successfull";
+    
+}
